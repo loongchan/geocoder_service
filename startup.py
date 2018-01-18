0e14a8a875cc46b3f1f426dfd5d68994d61e0b9d
loongchan <loongchan@gmail.com>
2018-01-18 14:59:58 -0800

    closes #7 - simplest way of doing this.

--- startup.py
+++ startup.py
@@ -11,8 +11,20 @@ from http.server import HTTPServer
 from apps.utils.get_request_handler import GetRequestHandler
 
 
-# initialize and start up server
-httpServer = HTTPServer(('', 8000), GetRequestHandler)
+# main entry point to start up Geocoding service
+if __name__ == "__main__":
 
-while True:
-    httpServer.handle_request()
+    #default port is 8000, but you can specify port by passing in integer as first argument to calling startup.py
+    port = 8000
+    if len(sys.argv) == 2:
+        try:
+            port = int(sys.argv[1])
+        except ValueError:
+            print("First argument must be an integer that specifies the port number.")
+            exit(1)
+
+    # initialize and start up server
+    httpServer = HTTPServer(('', port), GetRequestHandler)
+
+    while True:
+        httpServer.handle_request()
